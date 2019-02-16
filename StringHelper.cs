b12ca68be824e3693cc34186f8c08408a6937c2a
solodby <solodby@gmail.com>
2019-02-16 22:40:48 +0300

    v2

--- StringHelper.cs
+++ StringHelper.cs
@@ -6,10 +6,11 @@ namespace PadawansTask4
     {
         public static int GetVowelCount(string str)
         {
-            char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-                't', 'u', 'v', 'w', 'y', 'z' };
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
 
+            char[] array = str.ToLower().ToCharArray();
 
+            int vowelsCount = 0;
         }
     }
 }
