6e328d6badcd9ff03bff1c1669afd6847d8ad959
solodby <solodby@gmail.com>
2019-02-16 14:10:55 +0300

    v1

--- StringHelper.cs
+++ StringHelper.cs
@@ -6,8 +6,10 @@ namespace PadawansTask4
     {
         public static int GetVowelCount(string str)
         {
-            // put your code here
-            throw new NotImplementedException();
+            char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                't', 'u', 'v', 'w', 'y', 'z' };
+
+
         }
     }
 }
